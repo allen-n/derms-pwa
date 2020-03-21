6cdc55db523b66f511aa0d45dd5fce934b28d918
allen-n <allennikka@gmail.com>
2020-03-20 23:35:36 -0700

    Conversion to PWA complete. TODO: Add DERMS functionality.

---
+++ public/serviceWorker.js
@@ -0,0 +1,42 @@
+const version = 'v1/';
+const assetsToCache = [
+    '/',
+    '/src.7ed060e2.js',
+    '/src.7ed060e2.css',
+    '/manifest.webmanifest',
+    '/icon-128x128.3915c9ec.png',
+    '/icon-256x256.3b420b72.png',
+    '/icon-512x512.fd0e04dd.png',
+];
+
+// Cache the necessary files
+self.addEventListener('install', (event) => {
+    // Reasining src: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    self.skipWaiting(); // forces activation of current Service worker
+
+    // SW wont move on until 'caches' is resolved
+    event.waitUntil(
+        caches
+            .open(version + 'assetsToCache')
+            .then((cache) => cache.addAll(assetsToCache))
+            .then(() => console.log('assets cached')),
+    );
+});
+
+// Serve the cached files when the network fails
+self.addEventListener('fetch', (event) => {
+    if (event.request.method === 'GET') {
+        event.respondWith(
+            fetch(event.request).catch(() => {
+                return caches.match(event.request);
+            }),
+        );
+    }
+});
+
+/**
+ * Note:
+ Use of --no-content-hash in package.json makes parcel src
+ file prefixes dependent on location, not file contents,
+ so we don;t have to change 'assetsToCache' too often
+ */
